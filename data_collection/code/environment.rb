7235645826db24759b3bb81781ef92b7879be01e
Devin Gaffney <itsme@devingaffney.com>
2016-10-27 17:42:55 -0400

    Updates

--- data_collection/code/environment.rb
+++ data_collection/code/environment.rb
@@ -16,5 +16,6 @@ REDIS_SUBMISSIONS = Redis.new(db: 2)
 REDIS_COMMENTS = Redis.new(db: 3)
 MongoMapper.connection = Mongo::MongoClient.new("localhost", 27017, :pool_size => 25, :op_timeout => 600000, :timeout => 600000, :pool_timeout => 600000)
 MongoMapper.database = "reddit_diagnostics"
+Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/tasks/*.rb'].each {|file| require file }
