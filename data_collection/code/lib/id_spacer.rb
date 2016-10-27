ab05f1f2a162c0ba4d7ff145575938521e802f80
Devin Gaffney <itsme@devingaffney.com>
2016-10-27 17:40:03 -0400

    Updates

--- data_collection/code/lib/id_spacer.rb
+++ data_collection/code/lib/id_spacer.rb
@@ -7,7 +7,7 @@ class IdSpacer
       prev_last = `tail #{object_type.source_files[prev_index]}`.split("\n").last.to_i
       new_first = `head #{object_type.source_files[prev_index+1]}`.split("\n").first.to_i
       if new_first-prev_last != 1
-        to_insert << {id_start_range_inclusive: prev_last+1, id_end_range_inclusive: new_first-1, id_start_range_inclusive_36: base_10_to_36(prev_last+1), id_end_range_inclusive_36: base_10_to_36(new_first-1), width: (new_first-prev_last)-1, file: file}
+        to_insert << {id_start_range_inclusive: prev_last+1, id_end_range_inclusive: new_first-1, id_start_range_inclusive_36: base_10_to_36(prev_last+1), id_end_range_inclusive_36: base_10_to_36(new_first-1), width: (new_first-prev_last)-1, file: source_file}
       end
     end
     last_number = nil
@@ -28,7 +28,7 @@ class IdSpacer
       if !last_number.nil? && !next_number.nil? #then we are in the space now
         if next_number-last_number != 1 #then there's a gap
           width = (next_number-last_number)-1
-          to_insert << {id_start_range_inclusive: (next_number-width), id_end_range_inclusive: next_number-1, id_start_range_inclusive_36: base_10_to_36(next_number-width), id_end_range_inclusive_36: base_10_to_36(next_number-1), width: width, file: file} if (next_number-width) != next_number-1
+          to_insert << {id_start_range_inclusive: (next_number-width), id_end_range_inclusive: next_number-1, id_start_range_inclusive_36: base_10_to_36(next_number-width), id_end_range_inclusive_36: base_10_to_36(next_number-1), width: width, file: source_file} if (next_number-width) != next_number-1
         end
         last_number = next_number
         next_number = nil
