4cd6542db327a61a835bdd131698913b08189751
Devin Gaffney <itsme@devingaffney.com>
2016-10-27 17:33:50 -0400

    updates

--- data_collection/code/tasks/insert_keys_to_redis.rb
+++ data_collection/code/tasks/insert_keys_to_redis.rb
@@ -27,6 +27,7 @@ class InsertKeysToRedis
   
   #for each source file, determine if the file refers to submissions (posts) or comments. Then, walk through and store all ids.
   def perform(source_file)
+binding.pry
     metric_name = source_file.include?("submission") ? "submission_count" : "comment_count"
     redis_cli = source_file.include?("submission") ? REDIS_SUBMISSIONS : REDIS_COMMENTS
     CSV.foreach(source_file) do |row|
