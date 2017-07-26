599b98bce7199cb901aad40b773e74d893d6ef7d
Devin Gaffney <itsme@devingaffney.com>
2017-07-26 14:17:54 -0400

    updates

--- data_collection/code/tasks/insert_keys_to_redis.rb
+++ data_collection/code/tasks/insert_keys_to_redis.rb
@@ -1,5 +1,6 @@
 class InsertKeysToRedis
   include Sidekiq::Worker
+  sidekiq_options queue: :insert_keys
   ##hash_get_key_field, #hash_set, and #hash_get are all copied from http://redis.io/topics/memory-optimization
   #they efficiently store the data into Redis - this demonstration of the analysis used doesn't need much memory,
   #but in order to scan all of Reddit in memory, it requires 11GB of an RDB, which expands when actually loaded in memory.
@@ -7,43 +8,64 @@ class InsertKeysToRedis
   #within 64GB of ram, which was the limit available to the author. Please follow the tutorial at the above URL for 
   #actual implementation and replication
   def hash_get_key_field(key)
-      s = key.split(":")
-      if s[1].length > 2
-          {:key => s[0]+":"+s[1][0..-3], :field => s[1][-2..-1]}
-      else
-          {:key => s[0]+":", :field => s[1]}
-      end
+    if key.length > 3
+      {:key => key[0..-4], :field => key[-3..-1]}
+    else
+      {:key => "", :field => key}
+    end
   end
 
   def hash_set(r,key,value)
-      kf = hash_get_key_field(key)
-      r.hset(kf[:key],kf[:field],value)
+    kf = hash_get_key_field(key)
+    r.hset(kf[:key],kf[:field],value)
+  end
+  
+  def hash_m_set(r,keyval_map)
+    keyval_map.each do |key, values|
+      r.hmset(*[key,values.collect{|v| [v, "1"]}].flatten)
+    end
   end
 
   def hash_get(r,key)
-      kf = hash_get_key_field(key)
-      r.hget(kf[:key],kf[:field])
+    kf = hash_get_key_field(key)
+    r.hget(kf[:key],kf[:field])
   end
   
   #for each source file, determine if the file refers to submissions (posts) or comments. Then, walk through and store all ids.
-  def perform(source_file)
-    metric_name = source_file.include?("submission") ? "submission_count" : "comment_count"
-    redis_cli = source_file.include?("submission") ? REDIS_SUBMISSIONS : REDIS_COMMENTS
-    CSV.foreach(source_file) do |row|
-      hash_set(redis_cli,"obj:"+row[0],"1")
-      redis_cli.incr(metric_name)
+  def perform(year, file, data_type)
+    metric_name = data_type == "submissions" ? "submission_count" : "comment_count"
+    redis_cli = data_type == "submissions" ? REDIS_SUBMISSIONS : REDIS_COMMENTS
+    mapped = {}
+    count = 0
+    CSV.foreach("#{SETTINGS["download_path"]}/comments_ids/#{year}/#{file}") do |row|
+      mapped[row.first[0..-4]] ||= []
+      mapped[row.first[0..-4]] << row.first[-3..-1]
+      count += 1
+      if count > 100000
+        hash_m_set(r,mapped)
+        mapped = {}
+        count = 0
+      end
     end;false
+    #8.627484257263278 with 1k
+    #8.495383921663342 with 10k
+    #8.256297051107357 with 100k
+    #8.808857838182384 with 1m
+    #hash_set(redis_cli,row[0],"1")
+    #redis_cli.incr(metric_name)
   end
 
   #grab all relevant base_10 files and store all keys from file to memory in a hashmap
   def self.kickoff
-    Dir[File.dirname(__FILE__) + '/../../extracted_data/*.csv'].select{|x| x.include?("base_10") && x.include?("sorted")}.each do |source_file|
-      self.perform_async(source_file)
+    `ls #{SETTINGS["download_path"]}/comments_ids`.split("\n").each do |year|
+      `ls #{SETTINGS["download_path"]}/comments_ids/#{year}`.split("\n").each do |file|
+        InsertKeysToRedis.perform_async(year, file, "comments")
+      end
+    end
+    `ls #{SETTINGS["download_path"]}/submissions_ids`.split("\n").each do |year|
+      `ls #{SETTINGS["download_path"]}/submissions_ids/#{year}`.split("\n").each do |file|
+        InsertKeysToRedis.perform_async(year, file, "submissions")
+      end
     end
   end
-
-  def self.kickoff_noop
-    puts Dir[File.dirname(__FILE__) + '/../../extracted_data/*.csv'].select{|x| x.include?("base_10")}
-  end
 end
-
