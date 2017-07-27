fc35d545110c3e314c03d405c289720eb797b62e
Devin Gaffney <itsme@devingaffney.com>
2017-07-27 12:20:06 -0400

    Updates

--- data_collection/code/environment.rb
+++ data_collection/code/environment.rb
@@ -13,10 +13,10 @@ require 'redis'
 require 'mongo_mapper'
 require 'pry'
 require 'yaml'
+SETTINGS = YAML.load(File.read("config.yml")) rescue {"download_path" => "#{`pwd`.strip}/../data"}
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/tasks/*.rb'].each {|file| require file }
-SETTINGS = YAML.load(File.read("config.yml")) rescue {"download_path" => "#{`pwd`.strip}/../data"}
 REDIS_SUBMISSIONS = Redis.new(db: 2)
 REDIS_COMMENTS = Redis.new(db: 3)
 MongoMapper.connection = Mongo::MongoClient.new("localhost", 27017, :pool_size => 25, :op_timeout => 600000, :timeout => 600000, :pool_timeout => 600000)
--- data_collection/code/tasks/extract_fields.rb
+++ data_collection/code/tasks/extract_fields.rb
@@ -61,4 +61,3 @@ class ExtractFields
     end
   end
 end
-
