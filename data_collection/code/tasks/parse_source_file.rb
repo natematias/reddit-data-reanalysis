d3b29d248e7fab770f734df660fd1cd4d182bd8b
Devin Gaffney <itsme@devingaffney.com>
2017-08-01 10:26:01 -0400

    updates

--- data_collection/code/tasks/parse_source_file.rb
+++ data_collection/code/tasks/parse_source_file.rb
@@ -3,22 +3,26 @@ class CheckParentReferences
   sidekiq_options queue: :cpr
   def perform(year, file, data_type)
     current_rows = []
+    `mkdir #{SETTINGS["download_path"]}/#{data_type}_marked`
+    csv = CSV.open("#{SETTINGS["download_path"]}/#{data_type}_marked/#{year}/#{file}")
     File.foreach("#{SETTINGS["download_path"]}/#{data_type}_extracted/#{year}/#{file}") do |row|
       row = row.split(",").collect{|x| x.strip.gsub("\\", "").gsub("\"", "")}
       parent_type = row[4].split("_").first == "t3" ? "submissions" : "comments"
       metric_name = parent_type == "submissions" ? "submission_count" : "comment_count"
       redis_cli = parent_type == "submissions" ? REDIS_SUBMISSIONS : REDIS_COMMENTS
       found = InsertKeysToRedis.new.hash_get(redis_cli,row[5].to_s) == "1" ? true : false
-      current_rows << {found: found, source_year: year, source_file: file, time: Time.at(row[0].to_i), subreddit: row[1], comment_id: row[2], comment_id_int: row[3], parent_id: row[4], parent_id_int: row[5], parent_type: parent_type}
-      if current_rows.length >= 500
-        while Sidekiq::Queue.new.size > 1000
-          sleep(1)
-        end
-        ParentReference.collection.insert(current_rows) if current_rows.empty? == false
-        current_rows = []
-      end
+      csv << [found, year, file, Time.at(row[0].to_i), row[1], row[2], row[3], row[4], row[5], parent_type]
+#      current_rows << {found: found, source_year: year, source_file: file, time: Time.at(row[0].to_i), subreddit: row[1], comment_id: row[2], comment_id_int: row[3], parent_id: row[4], parent_id_int: row[5], parent_type: parent_type}
+#      if current_rows.length >= 500
+#        while Sidekiq::Queue.new.size > 1000
+#          sleep(1)
+#        end
+#        ParentReference.collection.insert(current_rows) if current_rows.empty? == false
+#        current_rows = []
+#      end
     end
-    ParentReference.collection.insert(current_rows) if current_rows.empty? == false
+    csv.close
+#    ParentReference.collection.insert(current_rows) if current_rows.empty? == false
   end
 
   def self.kickoff
